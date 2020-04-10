5e2a0ae8f931c7e0aaa07701869cf003af394187
Mashuq <mashuq@localhost.localdomain>
2020-04-10 10:41:40 +0600

    Updating package versions

--- config/neo4j.js
+++ config/neo4j.js
@@ -1,4 +1,9 @@
-let neo4j = require('neo4j-driver').v1;
-let driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "root"));
-let neo4jSession = driver.session();
+var neo4j = require('neo4j-driver');
+var driver = neo4j.driver(
+    'neo4j://localhost:7687',
+    neo4j.auth.basic('neo4j', 'root'),
+    { encrypted: 'ENCRYPTION_ON' }
+);
+var neo4jSession = driver.session()
+
 module.exports = neo4jSession;
